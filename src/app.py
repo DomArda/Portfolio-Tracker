3231b97f84d8a9a196bf1ca51530f6502d9b4535
Dante <Dominic.Ardavicius.2023@live.rhul.ac.uk>
2025-01-01 00:18:19 +0100

    ini methods - proof of concept

--- src/app.py
+++ src/app.py
@@ -1,7 +1,13 @@
 import api_interface
 import portfolio_gen
+from config import Config
+
+_ini = Config.Ini()
 
 API_key = input("Enter API Key:\n")
+
+_ini.set("Trading212", API_key)
+_ini.write()
 
 Trading212_Portfolio = portfolio_gen.Portfolio()
 Trading212_API = api_interface.Trading212(API_key)
